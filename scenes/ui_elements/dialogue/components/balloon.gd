390c7934c5744281dc50526c2961112f94167cf2
Fabricio Victor <fabriciovictorgarciaperez@gmail.com>
2026-03-26 01:19:33 -0500

    efecto kodak

--- scenes/ui_elements/dialogue/components/balloon.gd
+++ scenes/ui_elements/dialogue/components/balloon.gd
@@ -73,9 +73,11 @@ var _player_name: String = ""
 @onready var responses_menu: DialogueResponsesMenu = %ResponsesMenu
 
 @onready var talk_sound_player: AudioStreamPlayer = $TalkSoundPlayer
+@onready var overlay: AnimatedSprite2D = $Balloon/PanelContainer/AnimatedSprite2D
 
 
 func _ready() -> void:
+	overlay.play("idle")
 	balloon.hide()
 	Engine.get_singleton("DialogueManager").mutated.connect(_on_mutated)
 
