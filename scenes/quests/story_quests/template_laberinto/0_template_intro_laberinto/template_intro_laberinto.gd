661db1d95a1c6b14fe8e9fc6899725a3270230cf
Fabriciowwwwwwwwwww <fabriciovictorgarciaperez@gmail.com>
2026-03-31 10:08:12 -0500

    agregar minijuego

--- scenes/quests/story_quests/template_laberinto/0_template_intro_laberinto/template_intro_laberinto.gd
+++ scenes/quests/story_quests/template_laberinto/0_template_intro_laberinto/template_intro_laberinto.gd
@@ -2,5 +2,7 @@ extends Node2D
 @export var music_tracks: Array[AudioStream]
 
 func _ready() -> void:
+	var player = get_tree().get_first_node_in_group("player")
+	#player.get camrara zoom is 0.5
 	MusicManager.play_playlist(music_tracks)
 	await get_tree().process_frame
